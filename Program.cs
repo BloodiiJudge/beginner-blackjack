76a54eddfd950133e3fce0bae2e402470d0442be
BloodiiJudge <52362311+BloodiiJudge@users.noreply.github.com>
2019-12-03 11:34:00 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+class Program
+{
+    //Determines the number of players.
+    //protected int numPlayers;
+
+    //The number dealt to the players and the dealer.
+    //protected int playerNumber;
+    //protected int dealerNumber;
+    static void Main(string[] args)
+    {
+        //int numPlayers;
+        string input;
+
+        Console.Write("Number of players: \n");
+        input = Console.ReadLine();
+        Console.WriteLine("Input entered is '{0}'", input);
+        /**if(Int32.TryParse(input, out numPlayers))
+        {
+            numPlayers = Convert.ToInt32(input);
+        }
+        else
+        {
+            Console.WriteLine("Sorry, that is not a valid number.");
+        }**/
+    }
+}
+
